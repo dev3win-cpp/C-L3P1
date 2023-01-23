3d7e89a7a2802fc50eb9f1be2c1ee6fafbb73514
Tadeusz <tadeusz.potoniec@parhelium.com>
2023-01-23 11:24:30 +0100

    programs with loops: for, while and do while.

--- C++L3P1/main.cpp
+++ C++L3P1/main.cpp
@@ -3,12 +3,22 @@
 //  C++L3P1
 //
 //  Created by Tadeusz Potoniec on 23/01/2023.
-//
+//Based on https://www.youtube.com/watch?v=ErOzmh3BiXU&list=PLOYHgt8dIdoxx0Y5wzs7CFpmBzb40PaDo
 
 #include <iostream>
+#include <unistd.h> //The library used for countdown time
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+using namespace std;
+
+int main() {
+    for (int i=15;i>0;i--)
+    {
+        sleep(1);
+        cout<<i<<endl;
+    }
+    cout<<"BOOM"<<endl;
     return 0;
 }
+
+//++ OR i=i+1 to increment
+//for (int i=1;i<=10;i++)//What value i we start/when the loop should stop/what should happen with i after each loop
